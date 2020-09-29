10156354dd08e18529f12a4bab3bb1bde40876fd
Maxim Thomas <maxim.thomas.be@gmail.com>
2020-09-29 17:23:45 +0200

    update - add my name

--- siteBonneNouvelles/views/footer.php
+++ siteBonneNouvelles/views/footer.php
@@ -1,4 +1,5 @@
 		<div id="pied">
+			Maxim Thomas
 			<strong>Excellente journée qu'aujourd'hui le <?php echo $date ?></strong> :: 
 			<?php $time_end = microtime(true);
 				  $time = number_format(($time_end - $time_start)*1000,6);
@@ -8,7 +9,6 @@
 			<p id="copyright">Mise en page d'après
 				<a href="http://www.alsacreations.com/tutoriels/">Alsacréations</a>
 			</p>
-			<strong>Maxim Thomas</strong>
 		</div><!-- #pied -->
 	</div><!-- #global -->
 	</body>
